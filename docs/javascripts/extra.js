413af832edfa512f4e5b32903f53242961a7a122
Jeremy Walter <40613731+jeremywalter@users.noreply.github.com>
2020-07-20 15:43:11 -0400

    Update extra.js

--- docs/javascripts/extra.js
+++ docs/javascripts/extra.js
@@ -1,3 +1,1 @@
-<script async defer data-domain="cfde-training-and-engagement.readthedocs-hosted.com/en/jeremyfixes" src="https://plausible.io/js/plausible.js"></script>
-
 <script src="amplify.js" defer></script>
